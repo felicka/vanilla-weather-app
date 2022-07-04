9705e9bb8986facf59779a1ce022f97d97d05254
felicka <104525905+felicka@users.noreply.github.com>
2022-07-04 10:35:09 -0300

    Changed layout

--- src/index.js
+++ src/index.js
@@ -36,29 +36,17 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
 
-  let countryElement = document.querySelector("#country");
-  countryElement.innerHTML = response.data.sys.country;
-
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
-  let minElement = document.querySelector("#min");
-  minElement.innerHTML = Math.round(response.data.main.temp_min);
-
-  let maxElement = document.querySelector("#max");
-  maxElement.innerHTML = Math.round(response.data.main.temp_max);
-
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
-  let feelsElement = document.querySelector("#feels");
-  feelsElement.innerHTML = Math.round(response.data.main.feels_like);
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].main;
