7d6acc20f5efa0be4f704a73cdd6ef3f153cb504
felicka <104525905+felicka@users.noreply.github.com>
2022-07-10 09:42:59 -0300

    Added to forecast function

--- src/index.js
+++ src/index.js
@@ -30,15 +30,17 @@ function formatDay(timestamp) {
   return days[day];
 }
 //
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row"> `;
   let days = ["Mon", "Tue", "Wed"];
-  days.forEach(function(day){
-  forecastHTML =
-    forecastHTML +
-    `
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
     <div class="col-2">
       <div class="weather-forecast-date">${day}</div>
       <img
@@ -51,9 +53,17 @@ function displayForecast() {
         <span class="weather-forecast-temperature-min">12°</span>
       </div>
     </div>`;
+  });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-} });
+}
+
+function getForecast(coordinates) {
+  let apiKey = "f5fd95491fee61e88406e10ef8bac3ba4";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   celsiusTemperature = response.data.main.temp;
@@ -82,10 +92,12 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "fd8d1409866553a493730aeed9a355b9";
+  let apiKey = "5fd95491fee61e88406e10ef8bac3ba4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -125,4 +137,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Tbilisi");
-displayForecast();
